9b20f10251aa75213e2ed9d979a311434702d386
Jan Tuziak <jan.tuziak@gmail.com>
2023-07-12 17:35:22 +0200

    Trivial Test Framework

---
+++ test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ListmonkTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
