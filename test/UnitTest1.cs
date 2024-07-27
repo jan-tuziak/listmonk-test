b7cb1c6b6176355cc7f20e022ada7bb993c029a2
Jan Tuziak <jan.tuziak@gmail.com>
2024-07-27 12:11:05 +0200

    Install Playwright

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,16 +1,42 @@
+using System.Text.RegularExpressions;
+
 namespace ListmonkTest
 {
-    public class Tests
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class Tests : PageTest
     {
-        [SetUp]
-        public void Setup()
+        // [SetUp]
+        // public void Setup()
+        // {
+            
+        // }
+
+        // [Test]
+        // public void Test1()
+        // {
+        //     //Assert.Pass();
+        // }
+
+        [Test]
+        public async Task HasTitle()
         {
+            await Page.GotoAsync("https://playwright.dev");
+
+            // Expect a title "to contain" a substring.
+            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
         }
 
         [Test]
-        public void Test1()
+        public async Task GetStartedLink()
         {
-            Assert.Pass();
-        }
+            await Page.GotoAsync("https://playwright.dev");
+
+            // Click the get started link.
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
+
+            // Expects page to have a heading with the name of Installation.
+            await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
+        } 
     }
 }
